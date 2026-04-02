9dba79a5bdb526e8666e95f952a04065d379c269
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-02 13:34:57 -0500

    Add: generate-parentheses - submission-5

---
+++ Data Structures & Algorithms/generate-parentheses/submission-5.py
@@ -0,0 +1,33 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        tmp = []
+        res = []
+
+        def f(i,j):
+            if i==0 and j==0:
+                res.append(tmp.copy())
+                return
+            
+            if i==0:
+                tmp.append(")")
+                f(i,j-1)
+                tmp.pop()
+                return
+            
+            tmp.append("(")
+            f(i-1, j)
+            tmp.pop()
+
+            if j-1 >= i:
+                tmp.append(")")
+                f(i,j-1)
+                tmp.pop()
+            
+        
+        f(n,n)
+        ans = ["".join(seq) for seq in res]
+        print(res)
+        print("_")
+        print(ans)
+        return ans
+
