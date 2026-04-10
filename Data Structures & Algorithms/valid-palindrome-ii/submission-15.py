5d5ee1f0154c79217d39097c699975e3f3c6b8b1
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-10 15:44:43 -0500

    Add: valid-palindrome-ii - submission-15

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-15.py
@@ -0,0 +1,26 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        
+        def ispali(st):
+            l,r = 0, len(st)-1
+
+            while l<r:
+                if st[l]!=st[r]:
+                    return False
+                l+=1
+                r-=1
+            return True
+        
+        l, r = 0, len(s)-1
+        while l<r:
+            
+            if s[l] != s[r]:
+
+                if ispali(s[l:r])==False and ispali(s[l+1:r+1])==False:
+                    return False
+                
+            l+=1
+            r-=1
+        
+        return True
+
