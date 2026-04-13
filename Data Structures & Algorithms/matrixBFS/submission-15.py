96da618e3b86f2ac6be7c408b66b32c3da04764e
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-12 22:07:22 -0500

    Add: matrixBFS - submission-15

---
+++ Data Structures & Algorithms/matrixBFS/submission-15.py
@@ -0,0 +1,43 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]]) -> int:
+        R,C = len(grid), len(grid[0])
+
+        dis = [[-1]*C for i in range(R)]
+
+        dis[0][0] = 0
+
+        q = deque()
+        q.append((0,0))
+
+        dirc = [(-1,0), (1, 0), (0, -1), (0, 1)]
+
+        while q:
+            i,j = q.popleft()
+
+            for dx, dy in dirc:
+                u = i+dx
+                v = j+dy
+
+                if u>=0 and v>=0 and u<R and v<C and grid[u][v]==0 and (dis[u][v]==-1 or dis[u][v]> 1+dis[i][j]):
+                    q.append((u,v))
+                    dis[u][v] = 1 + dis[i][j]
+        
+        return dis[R-1][C-1]
+
+
+
+
+            
+
+
+
+
+
+        
+
+
+
+
+            
+
+        
