aeb60f16c70986964cbac6c9e0a22c573729469e
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-13 20:32:01 -0500

    Add: word-break - submission-13

---
+++ Data Structures & Algorithms/word-break/submission-13.py
@@ -0,0 +1,27 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+        memo = {}
+
+        def f(i):
+            if i==len(s):
+                return True
+            
+            if i in memo:
+                return memo[i]
+            
+            for w in wordDict:
+
+                if i+len(w) <= len(s) and s[i:i+len(w)] == w:
+
+                    if f(i+len(w)):
+
+                        memo[i] = True
+
+                        return True
+
+            
+            memo[i] = False
+            return False
+        
+        return f(0)
