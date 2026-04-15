d90e3d913813be4c8687b5e81f2bf8286f7a7e36
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-14 20:59:10 -0500

    Add: target-sum - submission-5

---
+++ Data Structures & Algorithms/target-sum/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+
+        def f(i, val):
+            if i==len(nums):
+                if val==0:
+                    return 1
+                return 0
+            
+            if (i,val) in memo:
+                return memo[(i,val)]
+            
+            plus = f(i+1, val+nums[i])
+            minus = f(i+1, val-nums[i])
+
+            res = plus+minus
+            
+            memo[(i,val)] = res
+            return res
+        
+        return f(0, target)
