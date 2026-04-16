95a2d52a7fbb99c72bc8e712481dab40fb773955
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-15 21:38:08 -0500

    Add: valid-sudoku - submission-12

---
+++ Data Structures & Algorithms/valid-sudoku/submission-12.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # Initialize 9 empty sets for each category
+        rows = [set() for i in range(9)]
+        cols = [set() for i in range(9)]
+        sqs = [set() for i in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                v = board[i][j]
+
+                if v == ".":
+                    continue
+                
+                idx = (i//3)*3 + (j//3)
+
+                if v in rows[i] or v in cols[j] or v in sqs[idx]:
+                    return False
+                
+                rows[i].add(v)
+                cols[j].add(v)
+                sqs[idx].add(v)
+        
+        return True
+
