e921237434c72d898c4d577196fb7a8f37e6345c
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-17 22:48:47 -0500

    Add: permutations-ii - submission-11

---
+++ Data Structures & Algorithms/permutations-ii/submission-11.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        tmp = []
+        res = []
+
+        nums.sort()
+
+        cnt = {}
+        for n in nums:
+            cnt[n] = cnt.get(n,0)+1
+        
+        def f():
+            if len(tmp)==len(nums):
+                res.append(tmp[:])
+                return
+            
+            for key, val in cnt.items():
+                
+                if cnt[key] > 0:
+                    cnt[key] -= 1
+                    tmp.append(key)
+                    f()
+                    tmp.pop()
+                    cnt[key] += 1
+
+        f()
+
+        return res
