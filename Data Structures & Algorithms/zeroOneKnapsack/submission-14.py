ce7786233397bdee946419fb50cf91dbd4c6c4be
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-18 22:53:30 -0500

    Add: zeroOneKnapsack - submission-14

---
+++ Data Structures & Algorithms/zeroOneKnapsack/submission-14.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maximumProfit(self, profit: List[int], weight: List[int], capacity: int) -> int:
+        R, C = len(weight), capacity+1
+        memo = [[-1]*(C) for i in range(R)]
+
+        return self.kp(0, capacity, profit, weight, memo)
+
+    def kp(self, i, cap, p, w, memo):
+        if i==len(p):
+            return 0
+        
+        if memo[i][cap] != -1:
+            return memo[i][cap]
+        
+        res = 0
+        notake = self.kp(i+1, cap, p, w, memo)
+        take = 0
+        if w[i] <= cap:
+            take = self.kp(i+1, cap-w[i], p, w, memo)+p[i]
+
+        res = max(notake, take)
+
+        memo[i][cap] = res
+        return res
+
