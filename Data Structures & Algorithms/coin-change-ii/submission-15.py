36f71da4cbec842854c8ecd52cc222d243a6ade6
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-04-27 22:20:03 -0500

    Add: coin-change-ii - submission-15

---
+++ Data Structures & Algorithms/coin-change-ii/submission-15.py
@@ -0,0 +1,23 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        
+
+        memo = [[-1]*(amount+1) for i in range(len(coins))]
+
+        def f(i, val):
+            if val==0:
+                return 1
+            if i==len(coins):
+                return 0
+            
+            if memo[i][val] != -1:
+                return memo[i][val]
+            
+            res = f(i+1, val)
+            if val-coins[i] >= 0:
+                res += f(i, val-coins[i])
+            
+            memo[i][val] = res
+            return res
+        
+        return f(0, amount)
