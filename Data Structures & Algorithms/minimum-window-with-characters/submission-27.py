2194cc6eecaf338b6afe7e5f990ab63ddbc45969
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-06-11 09:23:04 -0500

    Add: minimum-window-with-characters - submission-27

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-27.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t)>len(s):
+            return ""
+        cntT = {}
+        for c in t:
+            cntT[c] = cntT.get(c,0)+1
+        cntS = {}
+        residx, reslen = [-1,-1], float("inf")
+        l=0
+        have, need = 0, len(cntT)
+        for r in range(len(s)):
+            c = s[r]
+            cntS[c] = cntS.get(c,0)+1
+
+            if c in t and cntT[c]==cntS[c]:
+                have += 1
+            
+            while have == need:
+                if r-l+1 < reslen:
+                    reslen = r-l+1
+                    residx = [l,r]
+                getout = s[l]
+                cntS[getout]-=1
+                if getout in t and cntS[getout]+1==cntT[getout]:
+                    have-=1
+                l+=1
+            
+        
+        x,y = residx
+
+        return s[x:y+1] if reslen != float("inf") else ""
