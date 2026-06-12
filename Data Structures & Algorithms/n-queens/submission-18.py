9a9fc61d04a36ee9d47551fdb3048d70e74afbaa
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-06-12 09:42:29 -0500

    Add: n-queens - submission-18

---
+++ Data Structures & Algorithms/n-queens/submission-18.py
@@ -0,0 +1,42 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        b = [["."]*n for i in range(n)]
+        res = []
+        
+        def nq(row):
+            if row==n:
+                tmp = ["".join(r) for r in b]
+                res.append(tmp.copy())
+                return
+            for c in range(n):
+                if self.issafe(row,c,b):
+                    b[row][c]="Q"
+                    nq(row+1)
+                    b[row][c]="."
+            return
+        
+        nq(0)
+        return res
+
+    def issafe(self, row, col, b):
+        r,c = row, col
+        while r>=0:
+            if b[r][c]=="Q":
+                return False
+            r-=1
+        r,c = row, col
+        while r>=0 and c>=0:
+            if b[r][c]=="Q":
+                return False
+            r-=1
+            c-=1
+        r,c = row, col
+        while r>=0 and c<len(b[0]):
+            if b[r][c]=="Q":
+                return False
+            r-=1
+            c+=1
+        return True
+
+
+        
