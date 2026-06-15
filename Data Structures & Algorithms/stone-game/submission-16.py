e30f748912364faadc2cf9692595bb37be9c7c7f
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-06-15 18:26:38 -0500

    Add: stone-game - submission-16

---
+++ Data Structures & Algorithms/stone-game/submission-16.py
@@ -0,0 +1,25 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        memo = {}
+
+        def f(l,r):
+            if l>r:
+                return 0
+            
+            if (l,r) in memo:
+                return memo[(l,r)]
+            
+            even = (l-r+1)%2==0
+            takefirst = piles[l] if even else 0
+            takelast = piles[r] if even else 0
+
+            first = f(l+1,r)+takefirst
+            last = f(l,r-1)+takelast
+            res = max(first, last)
+            memo[(l,r)] = res
+            return res
+        
+        alice = f(0,len(piles)-1)
+        bob = sum(piles) - alice
+
+        return alice > bob
