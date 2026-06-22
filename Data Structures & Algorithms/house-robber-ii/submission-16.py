ab70239b814ab0981602e36408289589feb14ce4
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-06-21 23:44:33 -0500

    Add: house-robber-ii - submission-16

---
+++ Data Structures & Algorithms/house-robber-ii/submission-16.py
@@ -0,0 +1,26 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+
+        
+        def driver(arr):
+            memo = {}
+
+            def f(i):
+                if i>=len(arr):
+                    return 0
+                
+                if i in memo:
+                    return memo[i]
+                
+                res = max(f(i+1), f(i+2)+arr[i])
+                memo[i] = res
+                return res
+            
+            return f(0)
+        
+        first = driver(nums[:len(nums)-1])
+        last = driver(nums[1:])
+
+        return max(first, last)
