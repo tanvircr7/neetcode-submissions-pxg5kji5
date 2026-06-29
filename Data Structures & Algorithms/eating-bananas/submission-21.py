b6cdc41ef550ae46a13cf609e78f3e0f295dad0f
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-06-28 23:44:23 -0500

    Add: eating-bananas - submission-21

---
+++ Data Structures & Algorithms/eating-bananas/submission-21.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        
+        def daycnt(rate):
+            res = 0
+            for n in piles:
+                res += math.ceil(n/rate)
+            return res
+
+        l,r = 1, max(piles)
+        ans = r
+
+        while l<=r:
+            m = l+(r-l)//2
+            cnt = daycnt(m)
+
+            if cnt <= h:
+                ans = m
+                r = m-1
+            else:
+                l = m+1
+        
+        return ans
