5610e1cf4203213b23fb432110eb1118dc2231be
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-06-30 00:55:17 -0500

    Add: matchsticks-to-square - submission-23

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-23.py
@@ -0,0 +1,30 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        msk = matchsticks
+        if sum(matchsticks)%4 != 0:
+            return False
+        
+        msk.sort(reverse=True)
+        sides = [0]*4
+        sidelen = sum(msk)/4
+        memo = {}
+
+        def f(i):
+            if i==len(matchsticks):
+                return True
+
+            for side in range(4):
+                if sides[side]+msk[i] <= sidelen:
+                    sides[side] += msk[i]
+                    
+                    if f(i+1):
+                        return True
+                    sides[side] -= msk[i]
+                    
+                if sides[side]==0:
+                    break
+            
+
+            return False
+
+        return f(0)
