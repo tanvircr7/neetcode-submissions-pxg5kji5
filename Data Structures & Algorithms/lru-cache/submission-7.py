bd07b9fcf9ce6bdc1ea56105b63ca56eb4d4ca03
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-08-02 17:45:02 -0500

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev, self.next = None, None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.cap = capacity
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next, self.right.prev = self.right, self.left
+    
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.prev, node.next = prev, nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            res = self.cache[key]
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return res.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        if len(self.cache)>self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            print(f"removed {lru.key} for {key} - {value}")
+            print(self.cache)
+            del self.cache[lru.key]
+            print(self.cache)
