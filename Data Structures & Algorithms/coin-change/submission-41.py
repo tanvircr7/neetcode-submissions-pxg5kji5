3566c99c1ae2ced98dd03b64f30e7bbdd3cb4fa3
Mohammad Tanvir Hasan <34983412+tanvircr7@users.noreply.github.com>
2026-08-03 19:06:30 -0500

    Add: coin-change - submission-41

---
+++ Data Structures & Algorithms/coin-change/submission-41.py
@@ -0,0 +1,15 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        GG = amount + 1
+        memo = [GG for _ in range(amount+1)]
+        memo[0] = 0
+
+        for v in range(1, amount+1):
+            for c in coins:
+
+                if v-c>=0:
+                    memo[v] = min(memo[v], memo[v-c]+1)
+        
+
+        return memo[amount] if memo[amount]!=GG else -1
+
